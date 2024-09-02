d775ecb138d40be4a6a4e6470949a1a637ef79c1
alvmm <alv.mmartin@gmail.com>
2024-09-03 00:34:02 +0200

    cambios about

--- streamlit/views/about_page.py
+++ streamlit/views/about_page.py
@@ -12,3 +12,6 @@ Esperamos que disfrutes de esta experiencia y encuentres lo que estás buscando.
 """)
 
 st.image('https://www.ocu.org/-/media/ocu/images/home/vivienda%20y%20energia/gas%20y%20luz/confusion_electricas_1600x900.jpg?rev=daad7dee-5a08-4dce-a46b-934b395bd4bc&mw=480&hash=86D702E41831C25A7D8DB970B913FE25')
+
+
+#https://www.ocu.org/vivienda-y-energia/gas-luz/consejos/como-descifrar-la-factura-de-la-luz
