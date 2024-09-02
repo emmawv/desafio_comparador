74f3a5e2b24b289f5dfab5ffd9756110b3872dad
alvmm <alv.mmartin@gmail.com>
2024-09-02 23:18:56 +0200

    Cambios

---
+++ streamlit/streamlit.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+
+about_page = st.Page(
+    page = "views/about_page.py",
+    title = "Introducción: Entiende tu factura",
+    icon = ":material/account_circle:",
+    default= True
+)
+
+project_1_page = st.Page(
+    page = "views/sales_dashboard.py",
+    title = 'Dash Ventas',
+    icon = ":material/bar_chart:",
+)
+
+project_2_page = st.Page(
+    page = "views/grafica.py",
+    title = "Gráfico",
+    
+    icon = ":material/smart_toy:"
+)
+carga_factura = st.Page(
+
+
+    page = "views/carga.py",
+    title = 'Carga tu factura',
+    icon = ":material/add_link:"
+)
+
+formulario_factura = st.Page(
+
+    page = "views/formulario.py",
+    title = 'Rellena el formulario',
+    icon = ":material/add_link:"
+)
+
+
+
+pg = st.navigation(pages=[about_page, project_1_page, project_2_page, carga_factura, formulario_factura])
+
+pg.run()
--- streamlit/views/about_page.py
+++ streamlit/views/about_page.py
@@ -10,3 +10,5 @@ Su funcionamiento es muy sencillo: Carga tu factura y nuestro comparador te dar√
 
 Esperamos que disfrutes de esta experiencia y encuentres lo que est√°s buscando.
 """)
+
+st.image('https://www.ocu.org/-/media/ocu/images/home/vivienda%20y%20energia/gas%20y%20luz/confusion_electricas_1600x900.jpg?rev=daad7dee-5a08-4dce-a46b-934b395bd4bc&mw=480&hash=86D702E41831C25A7D8DB970B913FE25')
