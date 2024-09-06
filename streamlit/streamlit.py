5df4f7e8e57439378240bad5efb63a365ca39c2c
alvmm <alv.mmartin@gmail.com>
2024-09-06 20:29:59 +0200

    Reapply "Formulario terminado"
    This reverts commit f7ce34b2395bd2b0f49439427196728b08fffc47.

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 about_page = st.Page(
     page = "views/about_page.py",
-    title = "Introducción: Entiende tu factura",
+    title = "Entiende tu factura",
     icon = ":material/account_circle:",
     default= True
 )
@@ -14,6 +14,13 @@ project_1_page = st.Page(
     icon = ":material/bar_chart:",
 )
 
+g = st.Page(
+    page = "views/g.py",
+    title = "Gráfico",
+    
+    icon = ":material/smart_toy:"
+)
+
 project_2_page = st.Page(
     page = "views/grafica.py",
     title = "Gráfico",
@@ -22,18 +29,18 @@ project_2_page = st.Page(
 )
 carga_factura = st.Page(
 
-
     page = "views/carga.py",
     title = 'Carga tu factura',
     icon = ":material/add_link:"
 )
 
-formulario_factura = st.Page(
+formulario = st.Page(
 
     page = "views/formulario.py",
     title = 'Rellena el formulario',
     icon = ":material/add_link:"
 )
+
 camara = st.Page(
 
     page = "views/camara.py",
@@ -41,6 +48,6 @@ camara = st.Page(
     icon = ":material/add_link:"
 )
 
-pg = st.navigation(pages=[about_page, project_1_page, project_2_page, carga_factura, formulario_factura, camara])
+pg = st.navigation(pages=[about_page, project_1_page, project_2_page, carga_factura, formulario , camara, g])
 
 pg.run()
