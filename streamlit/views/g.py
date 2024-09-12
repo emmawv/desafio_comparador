ba64b3bd3df967a8810735b267384ce7982b18b0
alvmm <alv.mmartin@gmail.com>
2024-09-12 18:25:00 +0200

    Cambios casi finales

--- streamlit/views/g.py
+++ streamlit/views/g.py
@@ -127,32 +127,3 @@ from datetime import time
 from datetime import date
 
 
-
-# Cargar el conjunto de datos Iris
-df = pd.read_csv('../Datos/resultados.csv')
-df =df.drop_duplicates(subset=['tarifa'])
-# Configurar la aplicación Streamlit
-
-fecha_actual = date.today().strftime('%d-%m-%Y')
-st.title(f'Comparativa de mercado {fecha_actual}')
-
-
-
-
-fig2 = px.scatter(df, x="tarifa", 
-  y="total_factura", color_continuous_scale='sunset',
-  color="total_factura", hover_data=['tarifa', 'compania', 'total_factura'], custom_data=['compania'], labels={"tarifa": "Tarifa", "total_factura": "Precio factura"})
-fig2.update_layout(width=1500, height=1200,
-    
-    
-    coloraxis_showscale=False, title={
-        'text': "Comparativa compañias",
-        'y':0.95,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'})
-fig2.update_traces(hovertemplate='<b>Compañia:</b> %{customdata[0]} <br><b>Tarifa:</b> %{x} <br><b>Precio factura:</b> %{y:.2f}')
-fig2.update_xaxes(tickangle=45)
-
-
-st.plotly_chart(fig2)
